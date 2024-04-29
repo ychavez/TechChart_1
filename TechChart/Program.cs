9be252683cf6ccb8560f8ad26d71fee754df4b4c
ychavez <you400@gmail.com>
2024-04-29 10:02:13 -0600

    Agregar archivos de proyecto.

---
+++ TechChart/Program.cs
@@ -0,0 +1,70 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Linq;
+using System.Linq.Expressions;
+
+var summary = BenchmarkRunner.Run<BenchmarkTests>();
+[MemoryDiagnoser(false)]
+public class BenchmarkTests
+{
+    private const int Iterations = 1000;
+    private List<int> listData;
+    private IEnumerable<int> enumerableData;
+
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        listData = Enumerable.Range(0, Iterations).ToList();
+        enumerableData = Enumerable.Range(0, Iterations);
+    }
+
+
+    [Benchmark]
+    public void ListWhere()
+    {
+        var filtered = listData.Where(x => x % 2 == 0).ToList();
+        foreach (var item in filtered)
+        {
+            
+        }
+    }
+
+    [Benchmark]
+    public void EnumerableWhereList()
+    {
+        var filtered = enumerableData.Where(x => x % 2 == 0).ToList();
+        foreach (var item in filtered)
+        {
+
+        }
+    }
+
+    [Benchmark]
+    public void EnumerableWhere()
+    {
+        var filtered = enumerableData.Where(x => x % 2 == 0);
+        foreach (var item in filtered)
+        {
+
+        }
+    }
+
+
+
+    [Benchmark]
+    public void EnumerableWhereListWhere()
+    {
+        var filtered = enumerableData.ToList().Where(x => x % 2 == 0).ToList();
+        foreach (var item in filtered)
+        {
+
+        }
+    }
+
+
+
+
+
+
+}
